a3ffcc10353b64eb67e37e8a89061a5b7163fb9b
Limit <60055811+SxyLimit@users.noreply.github.com>
2020-07-17 19:30:50 +0800

    Update make.bat

--- bat/make.bat
+++ bat/make.bat
@@ -1,5 +1,4 @@
 @echo off
-
 set pro2=makedata.exe
 if NOT exist %pro2% (
  echo No makedata.exe
@@ -7,7 +6,6 @@ if NOT exist %pro2% (
  %0
  cls
 )
-
 cls
 set pro1=std.exe
 if NOT exist %pro1% (
@@ -15,12 +13,10 @@ if NOT exist %pro1% (
  pause>nul
  %0
 )
-
 cls
 set /p left=l:
 set /p right=r:
 cls
-
 for /L %%i in (%left%,1,%right%) do (
  echo Making data:%%i.in
  %pro2% >.\%%i.in
@@ -28,7 +24,6 @@ for /L %%i in (%left%,1,%right%) do (
  echo Making data:%%i.out
  %pro1% <.\%%i.in >.\%%i.out
  cls
-
 )
 echo Finish
 pause>nul
